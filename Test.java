7000412596955e6ddf92b1fe2fa9971c13ccb350
Anshul Sharma <anshul.s@cisinlabs.com>
2021-06-02 15:43:33 +0530

    Test added and updated

---
+++ Test.java
@@ -0,0 +1,46 @@
+class A
+{
+    void abc()
+    {
+        class B
+        {
+            void def()
+            {
+                class Local
+                {
+                    void ghi()
+                    {
+                        System.out.println(4);
+
+                    }
+                }
+                Local l =new Local(){
+                    void ghi()
+                    {
+                        System.out.println("inside anonymous");
+                    }
+                };
+                System.out.println(3);
+                l.ghi();
+                
+            }
+        }
+        B b=new B();
+        System.out.println(2);
+        b.def();
+
+    }
+}
+class Test
+{
+    public static void main(String ar[])
+    {
+        A a=new A();
+
+        System.out.println(1);
+        a.abc();
+
+    }
+}
+
+// test also updated
